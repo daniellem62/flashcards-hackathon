784444dce7216da015271f228b7ea4dbdce397ac
Sergio <sergeh96@gmail.com>
2025-01-23 13:46:07 +0000

    First commit. Ticket #1 done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* import { useState } from "react"; */
+
+import "./index.css";
+
+function App() {
+  /* const [count, setCount] = useState(0); */
+
+  return <div></div>;
+}
+
+export default App;
