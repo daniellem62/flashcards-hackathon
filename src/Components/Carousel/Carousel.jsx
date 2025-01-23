a25aa8e57caf038efbbb5274ec2b76272ca08045
Sergio <sergeh96@gmail.com>
2025-01-23 21:17:12 +0000

    progress on #12 shuffle. Come back to it tomorrow

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,27 +1,22 @@
 /* eslint-disable react/prop-types */
 // Aim is to build a carousel component that can be used to display images in a carousel format.
 // The component should be able to take in an array of images and display them in a carousel format.
-// The component should have the following features:
-// 1. Display the first three images in the array by default
-// 2. Display the next three images when the right arrow is clicked
-// 3. Display the previous three images when the left arrow is clicked
-// 4. The carousel should loop back to the first image when the right arrow is clicked on the last image
-// 5. The carousel should loop back to the last image when the left arrow is clicked on the first image
-// 6. The carousel should display the images in a horizontal format
-// 7. The carousel should have a fixed height and width
-// 8. The carousel should have a border around it
+// https://stackademic.com/blog/mastering-react-carousel-building-dynamic-image-sliders and copilot
 
 import { useState } from "react";
 import Flashcard from "../Flashcard/Flashcard";
 import styles from "./Carousel.module.css";
 
+// The component should have the following features:
 function Carousel({ existingCards }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // 1. Display the first three images in the array by default
+  // 2. Display the next three images when the right arrow is clicked
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 3) % existingCards.length);
   };
-
+  // 3. Display the previous three images when the left arrow is clicked
   const handlePrev = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex - 3 < 0 ? existingCards.length - 3 : prevIndex - 3
@@ -29,6 +24,20 @@ function Carousel({ existingCards }) {
   };
 
   const displayedCards = existingCards.slice(currentIndex, currentIndex + 3);
+
+  // LOGIC FOR SHUFFLING (fisher yates algorithm. Used in 3rd week hackathon)
+  // incomplete. come back to tomorrow
+  function shuffleArray(array) {
+    let shuffledArray = array.slice(); // create a copy
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  }
 
   return (
     <div>
