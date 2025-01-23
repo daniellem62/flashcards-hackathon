bb087b6bcf6d46e0355e6ecdf10edc86cb8db52f
Sergio <sergeh96@gmail.com>
2025-01-23 15:56:04 +0000

    #7 complete. State is handled properly for flipping cards

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable react/prop-types */
 import styles from "./Flashcard.module.css";
+import { useState } from "react";
 
 function Flashcard({ question, answer }) {
+  // Initialise state as false, as cards will render question pre flip
+  const [flipped, setFlipped] = useState(false);
+
+  // Function that "toggles" between flipped or not flipped
+  function handleFlip() {
+    // Sets flipped to true upon first click
+    setFlipped(!flipped);
+  }
+  // Runs the handleFlip function when clicked
   return (
-    <div className={styles.card}>
-      <div>{question}</div>
-      <p>{answer}</p>
-    </div>
+    <div className={styles.card} onClick={handleFlip}>
+      {flipped ? question : answer}
+    </div> // Ternary operator that renders either question or answer depending on state of flipped
   );
 }
 
