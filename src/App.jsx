9db9fc15a33283f4588721fb340a01919efc0882
Sergio <sergeh96@gmail.com>
2025-01-23 14:10:49 +0000

    progress on card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 /* import { useState } from "react"; */
 
 import "./index.css";
+import Flashcard from "./Components/Flashcard/Flashcard";
 
 function App() {
   /* const [count, setCount] = useState(0); */
 
-  return <div></div>;
+  return (
+    <div>
+      <Flashcard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Flashcard/Flashcard.jsx
@@ -0,0 +1,7 @@
+import styles from "./Flashcard.module.css";
+
+function Flashcard() {
+  return <div className={styles.card}>Test</div>;
+}
+
+export default Flashcard;
