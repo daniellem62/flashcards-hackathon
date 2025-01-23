ca1b02471d7dd279ccc91d67fdd7997750046e23
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-01-23 14:08:04 +0000

    Completed ticket #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 /* import { useState } from "react"; */
-
+import Header from "./components/header/header.jsx";
 import "./index.css";
 
 function App() {
   /* const [count, setCount] = useState(0); */
 
-  return <div></div>;
+  return (
+  <div>
+    <Header />
+  </div>
+  )
 }
 
 export default App;
