fedfdd1f4d4c4eb1a21e56b62a59c43e2f8e0981
Nick Goh <166708778+newmaldenite@users.noreply.github.com>
2025-01-23 14:28:37 +0000

    adds Footer component, footer basic positioning in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 /* import { useState } from "react"; */
-
+import Footer from "./Components/Footer/footer.jsx"
 import "./index.css";
 
 function App() {
   /* const [count, setCount] = useState(0); */
 
-  return <div></div>;
+  return <div> <Footer /> </div>;
 }
 
 export default App;
---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+
+
+function Footer() {
+    console.log("I am rendering");
+    return <div classname = "footer">
+      This is the bottom of the page.
+    </div>
+  };
+
+export default Footer;
