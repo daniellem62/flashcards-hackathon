9f17a6a49203d20e0ac0713933ecd43b89816398
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-01-23 18:28:07 +0000

    Added some styling using motion and css

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   console.log("I am rendering");
-  return <div className="footer">This is the bottom of the page.</div>;
+  return <div className="footer">Copyright Team 8 Mates</div>;
 }
 
 export default Footer;
